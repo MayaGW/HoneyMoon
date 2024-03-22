6828de997a362b542ae5351c822d07935157a602
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-03-22 10:25:26 +0100

    Initial Commit

---
+++ HoneyMoon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HoneyMoon
+//
+//  Created by Maya Ghamloush on 22/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HoneyMoon/HoneyMoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneyMoonApp.swift
+//  HoneyMoon
+//
+//  Created by Maya Ghamloush on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct HoneyMoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
